1e81174fdc670e16ec16115dc5c1a45df1745942
diegojaasiel <89817510+diegojaasiel@users.noreply.github.com>
2023-09-28 15:51:25 -0600

    S
    S

---
+++ mail.php
@@ -0,0 +1,67 @@
+<?php
+require 'vendor/autoload.php';
+
+// Configura la API Key de SendGrid
+$apiKey = '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';
+
+// Función para enviar el correo
+function enviarCorreo() {
+    $nombreEmpresa = $_POST['NombreEmpresa'];
+    $idCliente = $_POST['IdCode'];
+    $ciudadDestino = $_POST['CiudadDestino'];
+    $oficinaDestino = $_POST['OficinaDestino'];
+    $fechaHoraEntrega = $_POST['FechaHoraEntrega'];
+    $fechaHoraDevolucion = $_POST['FechaHoraDevolucion'];
+    $nombreConductor = $_POST['NombreConductor'];
+    $conductorAdicional = isset($_POST['ConductorAdicional']) ? "Sí" : "No";
+    $categoriaAuto = $_POST['CategoriaAuto'];
+    $metodoPago = $_POST['MetodoPago'];
+    $garantia = $_POST['Garantia'];
+
+    // Construye el cuerpo del correo
+    $mensaje = "
+    NOMBRE DE LA EMPRESA: $nombreEmpresa
+    ID CLIENTE: $idCliente
+    CIUDAD: $ciudadDestino
+    OFICINA: $oficinaDestino
+    FECHA Y HORA DE ENTREGA: $fechaHoraEntrega
+    FECHA Y HORA DE DEVOLUCIÓN: $fechaHoraDevolucion
+    NOMBRE DEL CONDUCTOR: $nombreConductor
+    CONDUCTOR ADICIONAL: $conductorAdicional
+    CATEGORÍA DE AUTO: $categoriaAuto
+    MÉTODO DE PAGO: $metodoPago
+    GARANTÍA: $garantia
+    ";
+
+    // Configura el objeto de correo
+    $email = new \SendGrid\Mail\Mail();
+    $email->setFrom("diego.grajeda@alamo.com.mx", "Tu Nombre");
+    $email->setSubject("Reserva para $nombreEmpresa");
+    $email->addTo("diego.jaasiel.grajeda.garcia@gmail.com", "Destinatario");
+    $email->addCc("diego.grajeda@alamo.com.mx", "Copia a");
+
+    // Establece el contenido del correo como texto
+    $email->addContent(
+        "text/plain",
+        $mensaje
+    );
+
+    // Envía el correo
+    $sendgrid = new \SendGrid($apiKey);
+    try {
+        $response = $sendgrid->send($email);
+        if ($response->statusCode() == 202) {
+            echo 'Correo enviado con éxito';
+        } else {
+            echo 'Error al enviar el correo';
+        }
+    } catch (Exception $e) {
+        echo 'Error al enviar el correo: ' . $e->getMessage();
+    }
+}
+
+// Escucha el envío del formulario
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    enviarCorreo();
+}
+?>
